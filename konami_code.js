af662182ae406f8e21b76351ff199fac7a25da05
Isuru <github email address>
2016-11-04 14:12:12 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,39 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init() {
-  // your code here
+var i = 0;
+
+ function init(e) {
+  //
+  // const key = parseInt(e.detail || e.which)
+  // if (key === code[i]){
+  //   i++;
+  //
+  //   if (i === code.length -1 ){
+  //     alert("Cheat code entered.");
+  //     i = 0;
+  //   }
+  // }else{
+  //   i = 0;
+  // }
+  //
+
 }
+
+// document.addEventListener('keydown', init);
+
+document.addEventListener('keydown',function init(e) {
+
+  const key = parseInt(e.detail || e.which)
+  if (key === code[i]){
+    i++;
+
+    if (i === code.length -1 ){
+      alert("Cheat code entered.");
+      i = 0;
+    }
+  }else{
+    i = 0;
+  }
+
+
+})
